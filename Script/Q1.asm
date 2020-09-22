d5acbb489f14c61299d34d205617158d97c28df5
nureazam <nur.azaz@northsouth.edu>
2020-09-22 10:11:43 +0600

    test commit

---
+++ Script/Q1.asm
@@ -0,0 +1,36 @@
+ .MODEL SMALL
+ .STACK 100H
+
+ .DATA
+    PROMPT_1  DB  'Enter the Lower Case Letter : $'
+    PROMPT_2  DB  0DH,0AH,'The Upper Case Letter is : $'
+
+ .CODE
+   MAIN PROC
+     MOV AX, @DATA                
+     MOV DS, AX
+
+     LEA DX, PROMPT_1            
+     MOV AH, 9
+     INT 21H
+
+     MOV AH, 1                   
+     INT 21H
+
+     MOV BL, AL                  
+
+     LEA DX, PROMPT_2            
+     MOV AH, 9
+     INT 21H
+
+     AND BL, 0DFH               
+                                
+
+     MOV AH, 2                   
+     MOV DL, BL
+     INT 21H
+
+     MOV AH, 4CH                 
+     INT 21H
+   MAIN ENDP 
+ END MAIN
---
+++ Script/Q3.asm
@@ -0,0 +1,32 @@
+.MODEL SMALL
+ .STACK 100H
+
+ .DATA
+    PROMPT  DB  'Reverse: $'
+
+ .CODE
+   MAIN PROC
+     MOV AX, @DATA             
+     MOV DS, AX
+
+     LEA DX, PROMPT              
+     MOV AH, 9
+     INT 21H
+
+     MOV CX, 26                
+
+     MOV AH, 2                  
+     MOV DL, 122               
+
+     @LOOP:                      
+       INT 21H                 
+
+       DEC DL                    
+       DEC CX                
+       
+     JNZ @LOOP                   
+
+     MOV AH, 4CH                
+     INT 21H
+   MAIN ENDP
+ END MAIN
